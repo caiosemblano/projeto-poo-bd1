9293f559816d24be6ab869de1114568f542029f4
caiosemblano <semblanocaio@gmail.com>
2026-06-03 21:33:57 -0300

    feat: initialize application and entry point with main menu display

--- src/main/java/br/inatel/GestaoPortifolioApplication.java
+++ src/main/java/br/inatel/GestaoPortifolioApplication.java
@@ -3,8 +3,14 @@ package br.inatel;
 
 public class GestaoPortifolioApplication {
     public static void main(String[] args) {
+        
+        GestaoPortifolioApplication app = new GestaoPortifolioApplication();
+        app.run();
 
+    }
 
-
+    public void run() {
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.exibirMenu();
     }
 }
