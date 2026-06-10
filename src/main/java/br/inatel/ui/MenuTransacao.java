1feb598e28659bdeebca1edcb11f5aad4280a228
caiosemblano <semblanocaio@gmail.com>
2026-06-10 18:13:20 -0300

    feat: Template for loops in CLI

--- src/main/java/br/inatel/ui/MenuTransacao.java
+++ src/main/java/br/inatel/ui/MenuTransacao.java
@@ -1,4 +1,34 @@
 package br.inatel.ui;
 
 public class MenuTransacao {
+
+    public void exibirMenu() {
+        int opcao;
+        do {
+            System.out.println("\n--- Menu de Transações ---");
+            System.out.println("1. Comprar Ativo");
+            System.out.println("2. Vender Ativo");
+            System.out.println("3. Listar Transações");
+            System.out.println("4. Voltar para o Menu Principal");
+            System.out.print("Escolha uma opção: ");
+           
+            switch (opcao) {
+                case 1:
+                    comprarAtivo();
+                    break;
+                case 2:
+                    venderAtivo();
+                    break;
+                case 3:
+                    listarTransacoes();
+                    break;
+                case 4:
+                    System.out.println("Voltando ao Menu Principal...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        } while (opcao != 4);
+    }
+
 }
