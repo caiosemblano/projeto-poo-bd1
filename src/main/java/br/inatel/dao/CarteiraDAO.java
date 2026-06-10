145a03d5c2f60eb159d6af12b1c1dcd77e1b54ef
caiosemblano <semblanocaio@gmail.com>
2026-06-10 16:13:59 -0300

    feat: implement CarteiraDAO for CRUD operations on the carteira table

--- src/main/java/br/inatel/dao/CarteiraDAO.java
+++ src/main/java/br/inatel/dao/CarteiraDAO.java
@@ -3,6 +3,7 @@ package br.inatel.dao;
 import br.inatel.model.Carteira;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarteiraDAO implements Repositorio<Carteira, Integer>{
@@ -59,16 +60,53 @@ public class CarteiraDAO implements Repositorio<Carteira, Integer>{
 
     @Override
     public List<Carteira> listarTodos() {
-        return List.of();
+        List<Carteira> carteiras = new ArrayList<>();
+        String sql = "SELECT * FROM carteira";
+        try {
+            pst = connection.prepareStatement(sql);
+            try (ResultSet rs = pst.executeQuery()) {
+                while (rs.next()) {
+                    Carteira carteira = new Carteira();
+                    carteira.setIdCarteira(rs.getInt("id_carteira"));
+                    carteira.setNomeCarteira(rs.getString("nome_carteira"));
+                    carteira.setDataCriacao(rs.getDate("data_criacao").toLocalDate());
+                    carteira.setDescricao(rs.getString("descricao"));
+                    carteira.setValorTotalInvestido(rs.getBigDecimal("valor_total_investido"));
+                    carteiras.add(carteira);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return carteiras;
     }
 
     @Override
-    public void atualizar(Carteira entidade) {
-
+    public void atualizar(Carteira carteira) {
+        String sql = "UPDATE carteira SET id_investidor = ?, nome_carteira = ?, data_criacao = ?, descricao = ?, valor_total_investido = ? WHERE id_carteira = ?";
+        try {
+            pst = connection.prepareStatement(sql);
+            pst.setInt(1, carteira.getInvestidor().getIdInvestidor());
+            pst.setString(2, carteira.getNomeCarteira());
+            pst.setDate(3, Date.valueOf(carteira.getDataCriacao()));
+            pst.setString(4, carteira.getDescricao());
+            pst.setBigDecimal(5, carteira.getValorTotalInvestido());
+            pst.setInt(6, carteira.getIdCarteira());
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public void deletar(Integer integer) {
-
+    public void deletar(Integer id_carteira) {
+        String sql = "DELETE FROM carteira WHERE id_carteira = ?";
+        try {
+            pst = connection.prepareStatement(sql);
+            pst.setInt(1, id_carteira);
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
