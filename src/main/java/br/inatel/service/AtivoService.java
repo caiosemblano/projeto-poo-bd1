1f99c33f324f37e7bb8f29f94461ed91d8b5600f
Pedro Henrique de Paiva Carvalho <pedro.paiva21205@gmail.com>
2026-06-10 20:27:11 -0300

    feat:Implementação do Menu ativo

--- src/main/java/br/inatel/service/AtivoService.java
+++ src/main/java/br/inatel/service/AtivoService.java
@@ -1,4 +1,45 @@
 package br.inatel.service;
 
+import br.inatel.dao.AtivoDAO;
+import br.inatel.model.Ativo;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class AtivoService {
+
+    private final AtivoDAO ativoDAO;
+
+    public AtivoService(AtivoDAO ativoDAO) {
+        this.ativoDAO = ativoDAO;
+    }
+
+    public void inserir(Ativo ativo) {
+        ativoDAO.inserir(ativo);
+    }
+
+    public Ativo buscarPorId(Integer id) {
+        return ativoDAO.buscarPorId(id);
+    }
+
+    public Ativo buscarPorSimbolo(String simbolo) {
+        return ativoDAO.buscarPorSimbolo(simbolo);
+    }
+
+    public List<Ativo> buscarPorTipo(String tipo) {
+        return ativoDAO.listarTodos().stream()
+                .filter(ativo -> ativo.getTipoAtivo().equalsIgnoreCase(tipo))
+                .collect(Collectors.toList());
+    }
+
+    public List<Ativo> listarTodos() {
+        return ativoDAO.listarTodos();
+    }
+
+    public void atualizar(Ativo ativo) {
+        ativoDAO.atualizar(ativo);
+    }
+
+    public void deletar(Integer id) {
+        ativoDAO.deletar(id);
+    }
 }
