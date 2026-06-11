58f4cc8087906e32a9ab05ceff9e06eb32d60ec7
caiosemblano <semblanocaio@gmail.com>
2026-06-10 21:16:17 -0300

    feat: implementar menu de objetivos

--- src/main/java/br/inatel/ui/MenuObjetivo.java
+++ src/main/java/br/inatel/ui/MenuObjetivo.java
@@ -1,4 +1,46 @@
 package br.inatel.ui;
 
+import java.util.Scanner;
+
 public class MenuObjetivo {
+
+    Scanner s;
+
+    public MenuObjetivo(Scanner scanner) {
+        this.s = scanner;
+    }
+
+    public void exibirMenu() {
+        int opcao = -1;
+        do {
+            System.out.println("\n--- Menu de Objetivos ---");
+            System.out.println("1. Criar Objetivo");
+            System.out.println("2. Listar Objetivos");
+            System.out.println("3. Atualizar Objetivo");
+            System.out.println("4. Excluir Objetivo");
+            System.out.println("5. Voltar para o Menu Principal");
+            System.out.print("Escolha uma opção: ");
+            opcao = s.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    // criarObjetivo();
+                    break;
+                case 2:
+                    // listarObjetivos();
+                    break;
+                case 3:
+                    // atualizarObjetivo();
+                    break;
+                case 4:
+                    // excluirObjetivo();
+                    break;
+                case 5:
+                    System.out.println("Voltando ao Menu Principal...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        } while (opcao != 5);
+    }
 }
